57c09304ee0eeb08184f41126789fa9cc5cd1709
danishmwasim <79856385+danishmwasim@users.noreply.github.com>
2021-04-01 16:59:14 -0500

    Renamed Project
    Renamed project to webstore

---
+++ webstore/wwwroot/inventory.js
@@ -0,0 +1,4 @@
+let productsContainer = document.getElementById("products");
+// //MVC, API - front end or back end, introduce yourself 
+// repository pattern 
+// C for complete - p for partial - N not complete  
